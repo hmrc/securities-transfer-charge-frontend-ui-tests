49419c0c3fdd13db28449e3ae22856f373e93ee0
karthik-a-j <141819159+karthik-a-j@users.noreply.github.com>
2026-02-23 09:28:22 +0000

    removed content validation as covered in Unit tests

--- src/test/scala/uk/gov/hmrc/ui/pages/KOPage.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/KOPage.scala
@@ -29,38 +29,22 @@ object KOPage extends BasePage {
   def validateLowClErrorMessage(): Unit = {
 
     val expectedHeading = "Before you use the service"
-    val expectedBody    =
-      "Before you can tell us about a securities transfer you need to provide more information to confirm your identity."
-
     val headingText = driver.findElement(By.cssSelector("h1")).getText.trim
-    val bodyText    = driver.findElement(By.cssSelector(".govuk-body")).getText.trim
-
+    
     assert(
       headingText == expectedHeading,
       s"Expected heading text '$expectedHeading' but found '$headingText'"
-    )
-    assert(
-      bodyText == expectedBody,
-      s"Expected heading text '$expectedBody' but found '$bodyText'"
     )
   }
 
   def validateCheckYourDetailsErrorMessage(): Unit = {
 
     val expectedHeading = "You must update the details on your government gateway account"
-    val expectedBody    =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
     val headingText = driver.findElement(By.cssSelector("h1")).getText.trim
-    val bodyText    = driver.findElement(By.cssSelector(".govuk-body")).getText.trim
-
+    
     assert(
       headingText == expectedHeading,
       s"Expected heading text '$expectedHeading' but found '$headingText'"
-    )
-    assert(
-      bodyText == expectedBody,
-      s"Expected heading text '$expectedBody' but found '$bodyText'"
     )
   }
 }
